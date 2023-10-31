d98ad6993de3896b06bf3c52d8585537b23fbe4a
Aarsha v joy <Aarsha@VL22KA002.am.amrita.edu>
2023-10-31 17:37:29 +0530

    welcome

---
+++ exp4/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+import Welcome from './components/Welcome'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="app">
+      <h1></h1></div>
+      <div>
+    <Welcome/>
+   
+     </div>
+    </>
+  )
+}
+
+export default App
